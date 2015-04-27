dbabe15e080574a8c8f35ee7e38b3acbe112e64b
Gerald <gerald.bv1@gmail.com>
2015-04-27 14:23:12 -0600

    Sitio funcionando
    Se agregaron las funcionalidades de agregar imagenes

--- js/scriptPagination.js
+++ js/scriptPagination.js
@@ -1,32 +1,55 @@
-<script type="text/javascript">
-$(function () {
-    $('.pglinks').on({
-        click: function (e) {
-       	 	ProcessPagination(this);
-        	return false;
-        }
-    }, '.pagination');
+$(document).ready(function() {                        
+    $.fn.pageMe = function(opts){
+        var $this = this,
+            defaults = {
+                perPage: 3
+            },
+            settings = $.extend(defaults, opts);                              
+        var listElement = $this;
+        var perPage = settings.perPage; 
+        var children = listElement.children();
+        var pager = $('.pager');                                
+        if (typeof settings.childSelector!="undefined") {
+            children = listElement.find(settings.childSelector);
+            console.log(children.size());
+        }                                
+        if (typeof settings.pagerSelector!="undefined") {
+            console.log(settings.pagerSelector);
+            pager = $(settings.pagerSelector);
+        }                                
+        var numItems = children.size();
+        var numPages = Math.ceil(numItems/perPage);
+        pager.data("curr",0);                                
+        var curr = 0;
+        while(numPages > curr){
+            $('<li><a href="#" class="page_link">'+(curr+1)+'</a></li>').appendTo(pager);
+            curr++;
+        }                                
+        pager.find('.page_link:first').addClass('active');                                
+        children.css('display', 'none');
+        children.slice(0, perPage).css('display', '');                                
+        pager.find('li a').click(function(){
+            var clickedPage = $(this).html().valueOf()-1;
+            goTo(clickedPage,perPage);
+        });                                
+        function previous(){
+            var goToPage = parseInt(pager.data("curr")) - 1;
+            if($('.active').prev('.page_link').length==true){
+                goTo(goToPage);
+            }
+        }                                 
+        function next(){
+            goToPage = parseInt(pager.data("curr")) + 1;
+            if($('.active_page').next('.page_link').length==true){
+                goTo(goToPage);
+            }
+        }                                
+        function goTo(page){
+            var startAt = page * perPage,
+                endOn = startAt + perPage;                                    
+            children.css('display','none').slice(startAt, endOn).css('display','');
+            pager.attr("curr",page);
+        }                    
+    };
+    $('#listStuff').pageMe({pagerSelector:'#listStuffPager',childSelector:'.col-md-4',perPage:3});                        
 });
-function ProcessPagination(obj) {
-    var id = $(obj).attr('id');
-    if (id != undefined) {
-    	var pid = null;
-    if (id.indexOf("pl_") != -1) {
-    	pid = id.replace("pl_", "");
-    }
-    else if (id.indexOf("pn_") != -1) {
-    	pid = id.replace("pn_", "");
-    }
-    else if (id.indexOf("pg_") != -1) {
-   	 	pid = id.replace("pg_", "");
-    }
-    else if (id.indexOf("pp_") != -1) {
-    	pid = id.replace("pp_", "");
-    }
-    else if (id.indexOf("p_") != -1) {
-    	pid = id.replace("p_", "");
-    }
-    	alert(pid + " linke selected");
-    }
-}
-</script>
